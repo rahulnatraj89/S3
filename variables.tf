6a2795cd31ffcd399d4bdba1467e56194d3ea0bc
rahulnatraj89 <122457791+rahulnatraj89@users.noreply.github.com>
2023-06-02 20:57:25 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,16 @@
 variable "buckets" {
     type = list(string)
-    default = ["sysops-tfm-lower-usac-org", "sysops-linux", "sysops-windows", "sysops-RHEL"]
+    default = ["name-1","name-2"]
   
+}
+variable "id" {
+  type = string
+  default = "12345678910"
 }
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-1"
+  default     = "us-east-1"
 }
 variable "force_destroy" {
   type    = string
@@ -16,8 +20,8 @@ variable "tags" {
   type    = map(string)
   default = {
     program = "sysops"
-    project = "li-nlad"
-    Environment = "lower"
+    project = "new"
+    Environment = "prod"
     managed_by  = "terraform"
 
   }
