2810dc134672aa36015296b83535dd737e7a0029
rahulnatraj89 <122457791+rahulnatraj89@users.noreply.github.com>
2023-06-02 20:58:56 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,12 +1,13 @@
 terraform {
+  required_version = ">= 0.13.1"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.52.0"
+      source  = "hashicorp/aws"
+      version = ">= 4.9"
     }
   }
 }
-
-provider "aws" {
-   region = "us-east-1"
+provider "aws" { 
+  region = var.aws_region
 }
